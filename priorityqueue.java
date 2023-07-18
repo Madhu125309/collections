f1b03c8b098606c285c8a02cf456cfbe78aec163
7997259422 <madhureddy85767@gmail.com>
2023-07-18 16:20:34 +0530

    hi

---
+++ priorityqueue.java
@@ -0,0 +1,12 @@
+
+import java.util.PriorityQueue;
+
+public class priorityqueue {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> a = new PriorityQueue<Integer>();
+        a.add(1000);
+        a.add(20);
+        a.add(10);
+        System.out.println(a);
+    }
+}
